f019ad873575d12f74a733f5fa3c1417d266a947
Natata <natnaelzelalem03@gmail.com>
2022-04-07 12:32:24 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
+
 #include <stdio.h>
 /**
- *main - Entry point
- *My hello, printf task
+ *main - entry point
+ *MY hello, printf task
  *Return: return 1 after program execution
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n);
+puts("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
+
