f5f3465f5aa81ed22983bd20cfedb00d70d05cee
Natata <natnaelzelalem03@gmail.com>
2022-04-14 10:09:34 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,7 @@
  * print_numbers - print numbers
  * Return: Always 0.
  */
+
 void print_numbers(void)
 {
 int i;
@@ -10,8 +11,8 @@ int i;
 i = 0;
 while (i < 10)
 {
-	_putchar (i + '0');
-	i++;
+_putchar (i + '0');
+i++;
 }
 
 _putchar ('\n');
