b280c389b67c91b4b15a4deef4e7fbbb0ed4bc1d
Natata <natnaelzelalem03@gmail.com>
2022-04-14 11:42:15 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers from 0 to 14 10 times
+ * more_numbers - prints 10 times the numbers, from 0 to 14
  * 
- * Return: void
+ * Return: Always 0.
  */
 
 void more_numbers(void)
