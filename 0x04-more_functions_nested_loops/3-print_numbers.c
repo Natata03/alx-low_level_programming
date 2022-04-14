656becdce2a5218b70a57ddb437fafaa59b89abc
Natata <natnaelzelalem03@gmail.com>
2022-04-14 10:39:41 -0700

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,22 @@
 #include "main.h"
+
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * print_numbers - prints the number zero to nine
+ *
+ * Return: Always 0 (success)
  */
+
 void print_numbers(void)
 {
+
 int i;
 
-i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-i++;
-}
+i = o;
+
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
 
 _putchar ('\n');
 }
+
